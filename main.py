6785c4e46d8d0456607fd60a0cdca1c2c0a6f53a
John Cho <yohancho316@gmail.com>
2021-07-27 16:57:37 -0700

    Changed Labels to Buttons & Included Indiv Funct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ################################# Python Imported Libraries ######################################
-import random
 import tkinter as tk
+import random
 from tkinter import *
 from tkinter import ttk
 from tkinter import PhotoImage
@@ -12,12 +12,10 @@ class Cube(tk.Tk):
   def __init__(self):
     super().__init__()
     self.title("John's GUI Cube Application")
-    self.geometry("300x180")
+    self.geometry("380x180")
     self.resizable(True,True)
-
     container = ttk.Frame(self)
     container.grid(padx=10,pady=10,sticky='EW')
-
     cube_frame = CubeFrame(container).grid(row=0,column=0,sticky='NSEW')
 
 class CubeFrame(ttk.Frame):
@@ -26,29 +24,30 @@ class CubeFrame(ttk.Frame):
     self.red_smiley = ImageTk.PhotoImage(Image.open('./images/red_smile.png'))
     self.white_smiley = ImageTk.PhotoImage(Image.open('./images/white_smile.png'))
     self.yellow_smiley = ImageTk.PhotoImage(Image.open('./images/yellow_smile.png'))
+    self.colors_list = [self.red_smiley,self.white_smiley,self.yellow_smiley]
 
     # Red Smiley Image Labels
-    self.red_smiley_1 = ttk.Label(self,image=self.red_smiley)
+    self.red_smiley_1 = ttk.Button(self,image=self.red_smiley,command=lambda:self.red_smiley_1.configure(image=self.colors_list[int(random.randint(0,2))]))
     self.red_smiley_1.grid(row=0,column=0)
-    self.red_smiley_2 = ttk.Label(self,image=self.red_smiley)
+    self.red_smiley_2 = ttk.Button(self,image=self.red_smiley,command=lambda:self.red_smiley_2.configure(image=self.colors_list[int(random.randint(0,2))]))
     self.red_smiley_2.grid(row=0,column=1)
-    self.red_smiley_3 = ttk.Label(self,image=self.red_smiley)
+    self.red_smiley_3 = ttk.Button(self,image=self.red_smiley,command=lambda:self.red_smiley_3.configure(image=self.colors_list[int(random.randint(0,2))]))
     self.red_smiley_3.grid(row=0,column=2)
 
     # White Smiley Image Labels
-    self.white_smiley_1 = ttk.Label(self,image=self.white_smiley)
+    self.white_smiley_1 = ttk.Button(self,image=self.white_smiley,command=lambda:self.white_smiley_1.configure(image=self.colors_list[int(random.randint(0,2))]))
     self.white_smiley_1.grid(row=1,column=0)
-    self.white_smiley_2 = ttk.Label(self,image=self.white_smiley)
+    self.white_smiley_2 = ttk.Button(self,image=self.white_smiley,command=lambda:self.white_smiley_2.configure(image=self.colors_list[int(random.randint(0,2))]))
     self.white_smiley_2.grid(row=1,column=1)
-    self.white_smiley_3 = ttk.Label(self,image=self.white_smiley)
+    self.white_smiley_3 = ttk.Button(self,image=self.white_smiley,command=lambda:self.white_smiley_3.configure(image=self.colors_list[int(random.randint(0,2))]))
     self.white_smiley_3.grid(row=1,column=2)
 
     # Yellow Smiley Image Labels
-    self.yellow_smiley_1 = ttk.Label(self,image=self.yellow_smiley)
+    self.yellow_smiley_1 = ttk.Button(self,image=self.yellow_smiley,command=lambda:self.yellow_smiley_1.configure(image=self.colors_list[int(random.randint(0,2))]))
     self.yellow_smiley_1.grid(row=2,column=0)
-    self.yellow_smiley_2 = ttk.Label(self,image=self.yellow_smiley)
+    self.yellow_smiley_2 = ttk.Button(self,image=self.yellow_smiley,command=lambda:self.yellow_smiley_2.configure(image=self.colors_list[int(random.randint(0,2))]))
     self.yellow_smiley_2.grid(row=2,column=1)
-    self.yellow_smiley_3 = ttk.Label(self,image=self.yellow_smiley)
+    self.yellow_smiley_3 = ttk.Button(self,image=self.yellow_smiley,command=lambda:self.yellow_smiley_3.configure(image=self.colors_list[int(random.randint(0,2))]))
     self.yellow_smiley_3.grid(row=2,column=2)
 
     # Image Label Buttons
@@ -60,19 +59,19 @@ class CubeFrame(ttk.Frame):
     for child in self.winfo_children():
       child.grid_configure(padx=5, pady=5)
 
-  # Change Row One Image Labels
+  # Change Entire Row One Image Buttons
   def change_row_one(self):
     self.red_smiley_1.configure(image=self.white_smiley)
     self.red_smiley_2.configure(image=self.white_smiley)
     self.red_smiley_3.configure(image=self.white_smiley)
 
-  # Change Row Two Image Labels
+  # Change Entire Row Two Image Buttons
   def change_row_two(self):
     self.white_smiley_1.configure(image=self.yellow_smiley)
     self.white_smiley_2.configure(image=self.yellow_smiley)
     self.white_smiley_3.configure(image=self.yellow_smiley)
 
-  # Change Row Three Image Labels
+  # Change Entire Row Three Image Buttons
   def change_row_three(self):
     self.yellow_smiley_1.configure(image=self.red_smiley)
     self.yellow_smiley_2.configure(image=self.red_smiley)
